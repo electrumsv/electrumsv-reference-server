1fe0a5efd1febfc9fd3cc81df9ccd8a5fd065bde
AustEcon <AustEcon0922@gmail.com>
2022-04-27 11:36:21 +1200

    Added a convenience script for manual REST API testing

---
+++ contrib/generate_client_key_data.py
@@ -0,0 +1,23 @@
+from bitcoinx import PrivateKey
+from datetime import datetime
+import json
+
+from esv_reference_server.keys import VerifiableKeyData
+
+CLIENT_IDENTITY_PRIVATE_KEY_HEX = "d468816bc0f78465d4833426c280166c3810ecc9c0350c5232b0c417687fbde6"
+CLIENT_IDENTITY_PRIVATE_KEY = PrivateKey.from_hex(CLIENT_IDENTITY_PRIVATE_KEY_HEX)
+
+
+def _generate_client_key_data() -> VerifiableKeyData:
+    iso_date_text = datetime.utcnow().isoformat()
+    message_bytes = b"http://server/api/account/metadata" + iso_date_text.encode()
+    signature_bytes = CLIENT_IDENTITY_PRIVATE_KEY.sign_message(message_bytes)
+    return {
+        "public_key_hex": CLIENT_IDENTITY_PRIVATE_KEY.public_key.to_hex(),
+        "message_hex": message_bytes.hex(),
+        "signature_hex": signature_bytes.hex()
+    }
+
+
+if __name__ == '__main__':
+    print(json.dumps(_generate_client_key_data()))
